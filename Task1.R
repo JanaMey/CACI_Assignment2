a5bd2382205d2def1d47f6d27b75392ca1c2136d
JanaMey <jana.meyer123@gmail.com>
2021-11-30 16:30:50 +0100

    Bezeichnung geändert

--- Task1.R
+++ Task1.R
@@ -1,3 +1,3 @@
 urlfile<-'https://raw.githubusercontent.com/JanaMey/CACI_Assignment2/main/QuestionaireData_CityTrips.csv?token=AIBBFR5OGL'
-dsin<-read.csv(urlfile)
-str(dsin)
+df<-read.csv(urlfile)
+str(df)
